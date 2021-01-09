a202feae336c33a9a201616fb878fd9e7054ff59
What should I put here? Help me out <74371276+xpqx@users.noreply.github.com>
2021-01-08 16:47:53 -0800

    timeObject 3

---
+++ deitel-java-apps/ch8/timeObject3/SimpleTimeTest.java
@@ -0,0 +1,9 @@
+package timeObject3;
+
+public class SimpleTimeTest {
+	public static void main(String[] args) {
+		SimpleTime time = new SimpleTime(15, 30, 19);
+		System.out.println(time.buildString() );
+	}
+
+}
