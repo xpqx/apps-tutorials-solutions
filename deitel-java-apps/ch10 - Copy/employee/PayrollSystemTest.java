373bde72d5315e538ef980966201ae541fb8d862
What should I put here? Help me out <74371276+xpqx@users.noreply.github.com>
2021-01-12 17:08:46 -0800

    add employee payroll system

---
+++ deitel-java-apps/ch10 - Copy/employee/PayrollSystemTest.java
@@ -0,0 +1,8 @@
+package ch10.employee;
+
+public class PayrollSystemTest {
+	public static void main(String[] args) {
+		BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee("Bob", "Lewis", "444-44-4444", 5000, .04, 3000);
+	}
+
+}
---
+++ deitel-java-apps/ch10 - Copy/employee/SalariedEmployee.java
@@ -0,0 +1,41 @@
+package ch10.employee;
+
+public class SalariedEmployee extends Employee {
+
+	private double weeklySalary;
+
+	public SalariedEmployee(String first, String last, String ssn, double salary) {
+		super(first, last, ssn);//pass to employee constructor
+		setWeeklySalary(salary);
+
+	}
+
+
+	//set salary
+	public void setWeeklySalary(double salary) {
+		if (salary>= 0.0) {
+			weeklySalary = salary;
+		} else {
+			throw new IllegalArgumentException("Weekly salary must be >= 0.0");
+		}//end method setWeeklySalary
+
+	}
+
+	public double getWeeklySalary() {
+		return weeklySalary;
+	}
+
+	public double earnings() {
+		return getWeeklySalary();
+	}
+
+
+	public String toString() {
+		return String.format("salaried employee: %s\n%s: $%,.2f", super.toString(), "weekly salary", getWeeklySalary());
+	}//end method toString
+
+
+
+
+
+}
